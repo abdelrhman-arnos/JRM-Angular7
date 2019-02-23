398aa6e7b35ce2f571acacb0c521a78e043990cf
Abdelrhman Mohamed <abdelrhman@codersea.com>
2019-02-24 01:45:42 +0200

    Countries

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as countries from '../../assets/countries.json';
 
 @Component({
   selector: 'app-ticket',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket.component.sass']
 })
 export class TicketComponent implements OnInit {
-
+  countries: any;
   constructor() { }
 
   ngOnInit() {
+    this.countries = countries;
   }
 
 }
