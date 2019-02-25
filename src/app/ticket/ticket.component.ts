4bf70bcb3095a932029ac5bee0bb23f458cb8ef5
Abdelrhman Mohamed <abdelrhman@codersea.com>
2019-02-25 20:38:58 +0200

    add angular forms, catch filter data form and ticket data form

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 import * as countries from '../../assets/countries.json';
 
 @Component({
+  name: 'CreateTicketPage',
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.sass']
 })
-export class TicketComponent implements OnInit {
+export class TicketComponent {
   countries: any;
-  constructor() { }
+  ticketForm = new FormGroup({
+    name: new FormControl(''),
+    type: new FormControl(''),
+    governorate: new FormControl(''),
+    city: new FormControl('EG'),
+    details: new FormControl('')
+  });
 
-  ngOnInit() {
+  constructor() {
     this.countries = countries;
   }
 
+  onSubmit() {
+    console.log(this.ticketForm);
+  }
 }
